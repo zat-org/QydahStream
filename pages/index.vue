c80986bf81eb3777576bd7b5f196ca5a0d8443e5
hazem hamdy <hh4morsy@gmail.com>
2024-09-05 16:03:10 +0300

    update aspect ratio

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="  bg-transparent  h-screen aspect-[9/16] flex flex-col justify-between " >
+  <div class="  bg-transparent   aspect-[9/16] flex flex-col justify-between " >
 <div class ="flex justify-between items-center  w-full">
   <div class="p-5 bg-red-500"></div>
   <div class="p-5 bg-red-500"></div> 
