f91c006e851ea1c81649d621f3c50a786c4f5e5b
hazem hamdy <hh4morsy@gmail.com>
2024-09-05 18:02:22 +0300

    return aaspect

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="  bg-transparent   w-[1080px] h-[1920px] flex flex-col justify-between " >
+  <div class="  bg-transparent    aspect-[9/16] flex flex-col justify-between " >
 <div class ="flex justify-between items-center  w-full">
   <div class="p-5 bg-red-500"></div>
   <div class="p-5 bg-red-500"></div> 
