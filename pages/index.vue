6ab6631d3a75d2c77d289904b7c71ea5439b203a
hazem hamdy <hh4morsy@gmail.com>
2024-09-05 18:07:05 +0300

    edit width

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="  bg-transparent    aspect-[9/16] flex flex-col justify-between " >
+  <div class="  bg-transparent  w-[1080px]  aspect-[9/16] flex flex-col justify-between " >
+    <!-- <div class="fixed top-0 right-[50%] "> player1</div>
+    <div class="fixed t" > player2</div>
+    <div> player1</div>
+    <div> player1</div> -->
+
 <div class ="flex justify-between items-center  w-full">
   <div class="p-5 bg-red-500"></div>
   <div class="p-5 bg-red-500"></div> 
