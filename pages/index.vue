1308582601c4e8a1017c0cf90e22fd7fb9620925
hazem hamdy <hh4morsy@gmail.com>
2024-09-05 15:57:45 +0300

    delete height

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="  bg-transparent w-[1080px] h-[1920px] flex flex-col justify-between " >
+    <div class="  bg-transparent w-[1080px]flex flex-col justify-between " >
   <div class ="flex justify-between items-center  w-full">
     <div class="p-5 bg-red-500"></div>
     <div class="p-5 bg-red-500"></div> 
