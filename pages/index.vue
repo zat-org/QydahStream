467c350074193ea6e45ada5529fc9abaf2d33581
hazem hamdy <hh4morsy@gmail.com>
2024-09-05 15:40:58 +0300

    update widtrh height

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="  bg-transparent w-[1080px] h-[1920px]flex flex-col justify-between " >
-<div class ="flex justify-between items-center  w-full">
-  <div class="p-5 bg-red-500"></div>
-  <div class="p-5 bg-red-500"></div> 
-</div>
-<div class="text-2xl font-bold flex  bg-blue-400 items-center justify-center">
-  <p>Hello world</p>
-</div>
-<div class ="flex justify-between items-center  w-full">
-  <div class="p-5 bg-red-500"></div>
-  <div class="p-5 bg-red-500"></div> 
-</div>
+
+    <div class="  bg-transparent w-[1080px] h-[1920px] flex flex-col justify-between " >
+  <div class ="flex justify-between items-center  w-full">
+    <div class="p-5 bg-red-500"></div>
+    <div class="p-5 bg-red-500"></div> 
   </div>
+  <div class="text-2xl font-bold flex  bg-blue-400 items-center justify-center">
+    <p>Hello world</p>
+  </div>
+  <div class ="flex justify-between items-center  w-full">
+    <div class="p-5 bg-red-500"></div>
+    <div class="p-5 bg-red-500"></div> 
+  </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
