399c85b2ca5dc78bebe03e11d6ebce026ec0a8dd
hazem hamdy <hh4morsy@gmail.com>
2024-09-05 14:48:44 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  ssr:false,
+  modules: ['@nuxtjs/tailwindcss'],
+  app:{
+    head:{
+      meta:[
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { charset: "utf-8" },
+
+      ]
+    }
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="  bg-transparent aspect-[9/16] flex flex-col justify-between " >
+<div class ="flex justify-between items-center  w-full">
+  <div class="p-5 bg-red-500"></div>
+  <div class="p-5 bg-red-500"></div> 
+</div>
+<div class="text-2xl font-bold flex  bg-blue-400 items-center justify-center">
+  <p>Hello world</p>
+</div>
+<div class ="flex justify-between items-center  w-full">
+  <div class="p-5 bg-red-500"></div>
+  <div class="p-5 bg-red-500"></div> 
+</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
