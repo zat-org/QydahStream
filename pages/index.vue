f123b8e0dd62071abe33f86b3fc1d6dff3d7e1a4
hazem hamdy <hh4morsy@gmail.com>
2024-09-07 11:55:16 +0300

    add player corner

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,31 @@
   <div class="p-5 bg-red-500"></div>
   <div class="p-5 bg-red-500"></div> 
 </div>
-<div class="text-2xl font-bold flex  bg-blue-400 items-center justify-center">
-  <p>Hello world</p>
+<div class="text-2xl font-bold flex flex-col   items-center justify-center h-full w-full">
+<!-- top -->
+<div class="grow flex items-start "> 
+<div class="p-5 text-white bg-black">
+  top
 </div>
+</div>
+<!-- right andleft -->
+<div class="flex justify-between w-full">
+  <div class="p-5 text-white bg-black"> left </div>
+  <div class="p-5 text-white bg-black"> right</div>
+
+</div>
+<!-- bottoom -->
+<div class="grow flex items-end ">
+<div class="p-5 text-white bg-black">
+  
+  bottom
+</div>
+</div>
+
+</div>
+
+
+
 <div class ="flex justify-between items-center  w-full">
   <div class="p-5 bg-red-500"></div>
   <div class="p-5 bg-red-500"></div> 
