3b3298287d4c0c060da35e9359461873de8ab03f
hazem hamdy <hh4morsy@gmail.com>
2024-09-07 15:44:48 +0300

    update ration replace width

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,31 @@
 <template>
-  <div class="mt-[67px] w-[640px] h-[1250px] m-0flex flex-col justify-between items-center  bg-green-500/55 " >
-    <!-- <div class="absolute top-0 right-[50%] p-5 bg-blue-400 "> player1</div>
-    <div class="absolute top-[100%] right-[50%] p-5 bg-blue-400" > player2</div>
-    <div class="absolute top-[50%] left-0 p-5 bg-red-400" > player3</div>
-    <div class="absolute top-[50%] left-[100%] p-5 bg-red-400"> player4 </div> -->
-<!-- 
-<div class ="flex justify-between items-center  w-full">
-  <div class="p-5 bg-red-500"></div>
-  <div class="p-5 bg-red-500"></div> 
-</div> -->
-<div class="text-2xl font-bold flex flex-col   items-center justify-center h-full w-full p-4 pb-10 ">
-<!-- top -->
-<div class="grow flex items-start "> 
-<div class="p-5 text-white bg-black">
-  top
-</div>
-</div>
-<!-- right andleft -->
-<div class="flex justify-between w-full">
-  <div class="p-5 text-white bg-black"> left </div>
-  <div class="p-5 text-white bg-black"> right</div>
+  <div
+    class="aspect-[9/18]  flex flex-col  border-2   ">
 
-</div>
-<!-- bottoom -->
-<div class="grow flex items-end ">
-<div class="p-5 text-white bg-black">
-  
-  bottom
-</div>
-</div>
+    <div
+      class="text-2xl font-bold flex flex-col items-center justify-center w-full h-full p-4 pb-10">
+      <!-- top -->
+      <div class="grow flex items-start">
+        <div class="p-5  bg-white">top</div>
+      </div>
+      <!-- right andleft -->
+      <div class="flex justify-between w-full">
+        <div class="p-5 bg-white">left</div>
+        <div class="p-5  bg-white">right</div>
+      </div>
+      <!-- bottoom -->
+      <div class="grow flex items-end">
+        <div class="p-5 bg-white">bottom</div>
+      </div>
+    </div>
 
-</div>
-
-
-
-<!-- <div class ="flex justify-between items-center  w-full">
+    <!-- <div class ="flex justify-between items-center  w-full">
   <div class="p-5 bg-red-500"></div>
   <div class="p-5 bg-red-500"></div> 
 </div> -->
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
