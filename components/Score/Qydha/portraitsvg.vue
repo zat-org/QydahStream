7d15fbe0980035bd9a1791e9fb326c04ed20264c
hazem hamdy <hh4morsy$gmail.com>
2024-10-17 01:35:39 +0300

    update

--- components/Score/Qydha/portraitsvg.vue
+++ components/Score/Qydha/portraitsvg.vue
@@ -48,7 +48,7 @@
     <div class="absolute  left-0   w-[95px] h-[95px]    " :style="{ 'top': 'calc(50% - 50px)', }"
       v-if="left && left.url && showPlayers">
       <div class="relative    w-full h-full ">
-        <img class=" absolute z-[10] bg-center bg-cover   w-[90%] h-[90%] top-[5px] left-[5px] "
+        <img class=" absolute z-[10] bg-center bg-cover  rounded-xl  w-[90%] h-[90%] top-[5px] left-[5px] "
           :style="{ 'background-image': `url(${left.url}) ` }" />
         <img class=" absolute z-[10]   w-full h-full  " src='/images/left-square.svg' />
 
@@ -57,7 +57,7 @@
     <div class="absolute right-0  w-[95px] h-[95px]  origin-center " style="top:calc(50% - 50px) "
       v-if="right && right.url && showPlayers">
       <div class="relative    w-full h-full">
-        <img class=" absolute z-[10] bg-center bg-cover   w-[90%] h-[90%] top-[5px] left-[5px] "
+        <img class=" absolute z-[10] bg-center bg-cover  rounded-xl  w-[90%] h-[90%] top-[5px] left-[5px] "
           :style="{ 'background-image': `url(${right.url}) ` }" />
         <img class=" absolute z-[10]  rotate-180 w-full h-full " src='/images/right-square.svg' />
 
@@ -66,7 +66,7 @@
     <div class=" absolute  bottom-0  w-[95px] h-[95px] origin-center " style="left:calc(50% - 45px)"
       v-if="bottom && bottom.url && showPlayers">
       <div class="relative   rounded-xl  w-full  h-full">
-        <img class=" absolute z-[10] bg-center bg-cover   w-[90%] h-[90%] top-[5px] left-[5px] "
+        <img class=" absolute z-[10] bg-center bg-cover  rounded-xl  w-[90%] h-[90%] top-[5px] left-[5px] "
           :style="{ 'background-image': `url(${bottom.url})` }" />
 
         <img class="absolute   z-[10]  rotate-270 w-full h-full" src='/images/bottom-square.svg' />
