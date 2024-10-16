d8f746d25c8a97afeb21c50cbf5c274e37e99a5f
hazem hamdy <hh4morsy$gmail.com>
2024-10-16 20:17:32 +0300

    update spaces

--- components/Score/Qydha/portraitsvg.vue
+++ components/Score/Qydha/portraitsvg.vue
@@ -45,20 +45,20 @@
       </div>
     </div>
 
-    <div class="absolute   left-0    origin-center " style="top:calc(50% - 50px) " v-if="left && left.url">
-      <div class="relative">
-        <img class="  z-[10] w-[75px] h-[100px]" src='/images/left-square.svg' />
-        <img :src="left?.url" class="w-[75px] h-[100px]  rounded-2xl " />
+    <div class="absolute   left-0 origin-center " style="top:calc(50% - 50px) " v-if="left && left.url">
+      <div class="relative w-[75px] h-[100px]">
+        <img class=" absolute z-[10] w-[75px] h-[100px]" src='/images/left-square.svg' />
+        <img :src="left?.url" class=" absolutew-[75px] h-[100px]  rounded-2xl " />
       </div>
     </div>
     <div class="absolute right-0   origin-center " style="top:calc(50% - 50px) " v-if="right && right.url">
-      <div class="relative">
-        <img class="  z-[10] w-[75px] h-[100px]" src='/images/right-square.svg' />
-        <img :src="right?.url" class="w-[75px] h-[100px]  rounded-2xl " />
+      <div class="relative w-[75px] h-[100px]">
+        <img class=" absolute z-[10] w-[75px] h-[100px]" src='/images/right-square.svg' />
+        <img :src="right?.url" class="absolute w-[75px] h-[100px]  rounded-2xl " />
       </div>
     </div>
     <div class=" absolute  bottom-0  origin-center " style="left:calc(50% - 32px)" v-if="bottom && bottom.url">
-      <div class="relative">
+      <div class="relative w-[75px] h-[100px]">
         <img class="absolute   z-[10] w-[75px] h-[100px]" src='/images/bottom-square.svg' />
         <img :src="bottom?.url" class="absolute w-[75px] h-[100px]  rounded-2xl " />
       </div>
