46a2f9969b56b743048b4eeb3a4fcd9738caabb5
hazem hamdy <hh4morsy$gmail.com>
2024-10-25 21:04:58 +0300

    update data

--- components/Cam/Top.vue
+++ components/Cam/Top.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="flex flex-col justify-between">
     <div class="flex justify-between items-center p-5">
-      <div class="relative w-[268px] h-[361px]" v-if="bottom && bottom.url">
+      <div class="relative w-[140px] h-[195px] " v-if="bottom && bottom.url">
           <img
-            class="w-[268px] h-[361px] absolute z-[10]"
+            class="w-[140px] h-[195px]  absolute z-[10]"
             src='/images/zat/usframe.svg'  />
           <img
             :src=" bottom?.url"
-            class="w-[268px] h-[361px] object-none object-center absolute rounded-2xl  top-[5px]" />
+            class="w-[140px] h-[187px]  object-none object-center absolute rounded-2xl  top-[5px]" />
         </div>
       <div v-else>
       </div>
 
 
 
-      <div class="relative w-[268px] h-[361px] " v-if="left && left.url">
+      <div class="relative w-[140px] h-[195px]  " v-if="left && left.url">
           <img
-            class="w-[268px] h-[361px]  absolute z-[10]"
+            class="w-[140px] h-[195px]  absolute z-[10]"
             src='/images/zat/themframe.svg' />
           <img
             :src=" left?.url"
-            class="w-[268px] h-[361px] absolute  object-none object-center rounded-2xl top-[5px]" />
+            class="w-[140px] h-[187px] absolute  object-none object-center rounded-2xl top-[5px]" />
         </div>
       <div v-else>
       </div>
@@ -31,26 +31,26 @@
 
 
 
-      <div class="relative w-[268px] h-[361px]" v-if="right && right.url">
+      <div class="relative w-[140px] h-[195px] " v-if="right && right.url">
           <img
-            class="w-[268px] h-[361px]  absolute z-[10] "
+            class="w-[140px] h-[195px]  absolute z-[10] "
             src='/images/zat/themframe.svg' />
           <img
             :src=" right?.url"
-            class="w-[268px] h-[361px] object-none object-center absolute top-[5px] rounded-2xl" />
+            class="w-[140px] h-[187px] object-none object-center absolute top-[5px] rounded-2xl" />
         </div>
       <div v-else>
       </div>
     
 
 
-      <div class="relative w-[268px] h-[361px]" v-if="top && top.url">
+      <div class="relative w-[140px] h-[195px]" v-if="top && top.url">
           <img
-            class="w-[268px] h-[361px]  absolute z-[10]"
+            class="w-[140px] h-[195px]  absolute z-[10]"
             src='/images/zat/usframe.svg'  />
           <img
             :src=" top?.url"
-            class="w-[268px] h-[361px] absolute  object-none object-center rounded-2xl top-[5px]" />
+            class="w-[140px] h-[187px] absolute  object-none object-center rounded-2xl top-[5px]" />
         </div>
       <div v-else>
       </div>
