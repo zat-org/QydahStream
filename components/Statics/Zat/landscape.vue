8f54b9c4394b99f94789a9410cdc0e4075f460f5
hazem hamdy <hh4morsy$gmail.com>
2024-11-03 19:23:19 +0200

    update data

--- components/Statics/Zat/landscape.vue
+++ components/Statics/Zat/landscape.vue
@@ -1,127 +1,127 @@
 <template>
 
-  <table class="w-full rounded-xl text-center table-fixed table-pin-rows my-3 shadow-lg">
+  <table class=" rounded-xl text-center table-fixed table-pin-rows my-3 shadow-lg">
     <thead>
-      <tr class="h-12 bg-gray-200 dark:bg-slate-700 text-xs">
-        <th class="text-gray-900 dark:text-white ">
+      <tr class="h-12 text-xs">
+        <th class=" ">
           {{ game!.usName }}
         </th>
         <th style="width: 20%">
           <UIcon name="fxemoji:squaredvs" class="text-2xl" />
         </th>
-        <th class="text-gray-900 dark:text-white ">
+        <th class="">
           {{ game!.themName }}
         </th>
       </tr>
     </thead>
-    <tbody>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+    <tbody>
+      <tr class="h-8 ">
+        <td class="font-semibold   ">
           {{ statusUs?.moshtaraSunCount }}
         </td>
         <td class="px-0 text-sm">مشترى صن</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.moshtaraSunCount }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.moshtaraHokmCount }}
         </td>
         <td class="px-0 text-sm">مشترى حكم</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.moshtaraHokmCount }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.wonMoshtaraCount }}
         </td>
         <td class="px-0 text-sm">مشتريات ناجحة</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.wonMoshtaraCount }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.lostMoshtaraCount }}
         </td>
         <td class="px-0 text-xs">مشتريات خسرانة</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.lostMoshtaraCount }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.sra }}
         </td>
         <td class="px-0 text-sm">سرا</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.sra }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.baloot }}
         </td>
         <td class="px-0 text-sm">بلوت</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.baloot }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.khamsen }}
         </td>
         <td class="px-0 text-sm">خمسين</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.khamsen }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.me2a }}
         </td>
         <td class="px-0 text-sm">مية</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.me2a }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.rob3ome2a }}
         </td>
         <td class="px-0 text-sm">أربعمية</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.rob3ome2a }}
         </td>
       </tr>
 
-      <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <tr class="h-8 ">
+        <td class="font-semibold ">
           {{ statusUs?.sunKaboot }}
         </td>
         <td class="px-0 text-xs">عدد الكبابيت صن</td>
-        <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+        <td class="font-semibold ">
           {{ statusThem?.sunKaboot }}
         </td>
       </tr>
 
-      <!-- <tr class="h-8 even:dark:bg-slate-500 odd:dark:bg-slate-600 even:bg-slate-50 odd:bg-slate-100">
-           <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+      <!-- <tr class="h-8 ">
+           <td class="font-semibold ">
              {{ statusUs?.hokmKaboot }}
            </td>
            <td class="px-0 text-xs">عدد الكبابيت حكم</td>
-           <td class="font-semibold text-md text-blue-950 dark:text-slate-50">
+           <td class="font-semibold ">
              {{ statusThem?.hokmKaboot }}
            </td>
          </tr> -->
