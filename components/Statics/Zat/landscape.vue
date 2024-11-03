4a1e165a1c4ed8e83d7de10f45ec3632ce24a7c3
hazem hamdy <hh4morsy$gmail.com>
2024-11-03 19:59:03 +0200

    update data

--- components/Statics/Zat/landscape.vue
+++ components/Statics/Zat/landscape.vue
@@ -1,132 +1,53 @@
 <template>
 
-  <table class=" rounded-xl text-center table-fixed table-pin-rows my-3 shadow-lg">
-    <thead>
-      <tr class="h-12 text-xs">
-        <th class=" ">
-          {{ game!.usName }}
-        </th>
-        <th style="width: 20%">
-          <UIcon name="fxemoji:squaredvs" class="text-2xl" />
-        </th>
-        <th class="">
-          {{ game!.themName }}
-        </th>
-      </tr>
-    </thead>
 
-    <tbody>
-      <tr class="h-8 ">
-        <td class="font-semibold   ">
-          {{ statusUs?.moshtaraSunCount }}
-        </td>
-        <td class="px-0 text-sm">مشترى صن</td>
-        <td class="font-semibold ">
-          {{ statusThem?.moshtaraSunCount }}
-        </td>
-      </tr>
+  <div class="grid grid-cols-3  overflow-hidden w-[1920px] h-[1080px]">
+    <div  class="flex flex-col gap-2 ">
+      <p id="usName"> 
+        {{ game!.usName }}
+      </p>
+      <p class="usData"> {{ statusUs?.moshtaraSunCount }}</p>
+      <p class="usData">{{ statusUs?.moshtaraHokmCount }}</p>
+      <p class="usData">{{ statusUs?.wonMoshtaraCount }}</p>
+      <p class="usData">{{ statusUs?.lostMoshtaraCount }} </p>
+      <p class="usData"> {{ statusUs?.sra }} </p>
+      <p class="usData"> {{ statusUs?.baloot }}</p>
+      <p class="usData"> {{ statusUs?.khamsen }}</p>
+      <p class="usData"> {{ statusUs?.me2a }}</p>
+      <p class="usData"> {{ statusUs?.rob3ome2a }}</p>
+      <p class="usData"> {{ statusUs?.sunKaboot }}</p>
+    </div>
+    <div class="flex flex-col gap-2  " id="titles">
+      <p></p>
+      <p>مشترى صن</p>
+      <p>مشترى حكم</p>
+      <p>مشتريات ناجحة</p>
+      <p> مشتريات خسرانة</p>
+      <p> سرا</p>
+      <p> بلوت</p>
+      <p> خمسين</p>
+      <p> مية</p>
+      <p> أربعمية</p>
+      <p> عدد الكبابيت صن</p>
+    </div>
+    <div class="flex flex-col gap-2 ">
 
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.moshtaraHokmCount }}
-        </td>
-        <td class="px-0 text-sm">مشترى حكم</td>
-        <td class="font-semibold ">
-          {{ statusThem?.moshtaraHokmCount }}
-        </td>
-      </tr>
+      <p id="themName">{{ game!.themName }}</p>
+      <p class="themData">{{ statusThem?.moshtaraSunCount }}</p>
+      <p class="themData">{{ statusThem?.moshtaraHokmCount }}</p>
+      <p class="themData">{{ statusThem?.wonMoshtaraCount }}</p>
+      <p class="themData">{{ statusThem?.lostMoshtaraCount }} </p>
+      <p class="themData"> {{ statusThem?.sra }}</p>
+      <p class="themData"> {{ statusThem?.baloot }}</p>
+      <p class="themData"> {{ statusThem?.khamsen }}</p>
+      <p class="themData"> {{ statusThem?.me2a }}</p>
+      <p class="themData"> {{ statusThem?.rob3ome2a }}</p>
+      <p class="themData"> {{ statusThem?.sunKaboot }}</p>
+    </div>
 
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.wonMoshtaraCount }}
-        </td>
-        <td class="px-0 text-sm">مشتريات ناجحة</td>
-        <td class="font-semibold ">
-          {{ statusThem?.wonMoshtaraCount }}
-        </td>
-      </tr>
 
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.lostMoshtaraCount }}
-        </td>
-        <td class="px-0 text-xs">مشتريات خسرانة</td>
-        <td class="font-semibold ">
-          {{ statusThem?.lostMoshtaraCount }}
-        </td>
-      </tr>
+  </div>
 
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.sra }}
-        </td>
-        <td class="px-0 text-sm">سرا</td>
-        <td class="font-semibold ">
-          {{ statusThem?.sra }}
-        </td>
-      </tr>
-
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.baloot }}
-        </td>
-        <td class="px-0 text-sm">بلوت</td>
-        <td class="font-semibold ">
-          {{ statusThem?.baloot }}
-        </td>
-      </tr>
-
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.khamsen }}
-        </td>
-        <td class="px-0 text-sm">خمسين</td>
-        <td class="font-semibold ">
-          {{ statusThem?.khamsen }}
-        </td>
-      </tr>
-
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.me2a }}
-        </td>
-        <td class="px-0 text-sm">مية</td>
-        <td class="font-semibold ">
-          {{ statusThem?.me2a }}
-        </td>
-      </tr>
-
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.rob3ome2a }}
-        </td>
-        <td class="px-0 text-sm">أربعمية</td>
-        <td class="font-semibold ">
-          {{ statusThem?.rob3ome2a }}
-        </td>
-      </tr>
-
-      <tr class="h-8 ">
-        <td class="font-semibold ">
-          {{ statusUs?.sunKaboot }}
-        </td>
-        <td class="px-0 text-xs">عدد الكبابيت صن</td>
-        <td class="font-semibold ">
-          {{ statusThem?.sunKaboot }}
-        </td>
-      </tr>
-
-      <!-- <tr class="h-8 ">
-           <td class="font-semibold ">
-             {{ statusUs?.hokmKaboot }}
-           </td>
-           <td class="px-0 text-xs">عدد الكبابيت حكم</td>
-           <td class="font-semibold ">
-             {{ statusThem?.hokmKaboot }}
-           </td>
-         </tr> -->
-    </tbody>
-  </table>
 
 </template>
 
@@ -149,14 +70,14 @@ const { sleep } = useSleep()
 onMounted(async () => {
   console.log("Hello 1 ")
   await sleep(1000)
-  gameStore.gameService.send({ type: "NEXT" });
-  console.log("Hello 2")
-  await sleep(1000)
+  // gameStore.gameService.send({ type: "NEXT" });
+  // console.log("Hello 2")
+  // await sleep(1000)
 
-  gameStore.gameService.send({ type: "TO_OUTRO" });
-  console.log("Hello 3")
-  await sleep(1000)
-  gameStore.gameService.send({ type: "CHECK_END" });
+  // gameStore.gameService.send({ type: "TO_OUTRO" });
+  // console.log("Hello 3")
+  // await sleep(1000)
+  // gameStore.gameService.send({ type: "CHECK_END" });
 
   console.log("bye")
 
