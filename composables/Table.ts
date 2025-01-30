1b47a3cf3bcff01c02c14309f5c98d7b2e92021d
hazem hamdy <hh4morsy$gmail.com>
2025-01-30 10:56:34 +0200

    use new way to get data

--- composables/Table.ts
+++ composables/Table.ts
@@ -1,6 +1,5 @@
 import { useFirestore, useDocument } from "vuefire";
-import { doc, setDoc } from "firebase/firestore";
-import { until } from "@vueuse/core";
+import { doc, getDoc, setDoc, onSnapshot } from "firebase/firestore";
 interface TableData {
   id: string;
   LeftPlayer: { top: string; left: string };
@@ -11,36 +10,34 @@ interface TableData {
 
 export const useTable = () => {
   const db = useFirestore();
+  const tableData = ref();
   const getOrCreateTable = async (tableId: string) => {
     const tableRef = doc(db, "tables", tableId);
-    const { data: tableDoc } = useDocument(tableRef);
-    await until(tableDoc).not.toBeNull();
-    const tableData = computed(() => {
-      if (tableDoc.value) {
-        console.log(" it was added ");
-        return tableDoc.value as TableData;
-      } else {
-        const defaultTableData: TableData = {
-          id: tableId,
-          PlayerImageWidth: 60,
-          LeftPlayer: { top: "", left: "0px" },
-          RightPlayer: { top: "", right: "0px" },
-          BottomPlayer: { bottom: "0px", left: "" },
-        };
-        defaultTableData.LeftPlayer.top = `calc(50% - ${defaultTableData.PlayerImageWidth/2}px)`;
-        defaultTableData.RightPlayer.top = `calc(50% - ${defaultTableData.PlayerImageWidth/2}px)`;
-        defaultTableData.BottomPlayer.left = `calc(50% - ${defaultTableData.PlayerImageWidth/2}px)`;
+    const docSnap = await getDoc(tableRef);
+    if (!docSnap.exists()) {
+      const defaultTableData: TableData = {
+        id: tableId,
+        PlayerImageWidth: 60,
+        LeftPlayer: { top: "", left: "0px" },
+        RightPlayer: { top: "", right: "0px" },
+        BottomPlayer: { bottom: "0px", left: "" },
+      };
+      defaultTableData.LeftPlayer.top = `calc(50% - ${
+        defaultTableData.PlayerImageWidth / 2
+      }px)`;
+      defaultTableData.RightPlayer.top = `calc(50% - ${
+        defaultTableData.PlayerImageWidth / 2
+      }px)`;
+      defaultTableData.BottomPlayer.left = `calc(50% - ${
+        defaultTableData.PlayerImageWidth / 2
+      }px)`;
+      await setDoc(tableRef, defaultTableData);
+    }
 
-        // Create new table if it doesn't exist
-        try {
-          setDoc(tableRef, defaultTableData);
-        } catch (e) {
-          console.log(e);
-        }
+    const unsubscribe = onSnapshot(tableRef, (doc) => {
+      tableData.value = doc.data();
+    });
 
-        return defaultTableData;
-      }
-    }).value;
     return tableData;
   };
 
