db713e7b3b40ac3c432cfe15393cfb8d8925d81c
hazem hamdy <hh4morsy$gmail.com>
2025-02-02 04:36:33 +0200

    update winner portrait

--- components/winner/Qydha/portrait.vue
+++ components/winner/Qydha/portrait.vue
@@ -1,63 +1,102 @@
 <template>
-  <div class="winner-comp  h-[300] w-[400px] mt-[200px] " ref="winnerComp">
-    <div  class="absolute w-[400px] h-[300px] flex justify-center  items-center top-[105px]  z-[10] " id="winner-data" ref="winnerData">
+  <div
+    class="winner-comp h-[300] w-[400px] mt-[200px]"
+    ref="winnerComp"
+    style="
+      width: 1080px;
+      height: 1920px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+    "
+  >
+    <div
+      class="absolute flex justify-center items-center z-[10]"
+      id="winner-data"
+      ref="winnerData"
+      style="opacity: 2; margin-top: 445px; font-size: 4rem; line-height: 6rem"
+    >
       <p id="winner-name" v-if="winner">
         {{ winner.name }}
       </p>
     </div>
-    <div class="absolute h-[300px] w-[400px] flex justify-center gap-5 items-center">
-      <div class=" relative     ">
-        <div class=" absolute  w-[120px] h-[100px]  left-[20px] top-[-45px] rotate-[15deg] z-[-2]  ">
+    <div
+      class="absolute flex justify-center gap-5 items-center"
+      style="width: 50%; height: 50%"
+    >
+      <div
+        class="relative"
+        style="
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        "
+      >
+        <div
+          class="absolute rotate-[15deg] z-[-2]"
+          style="
+            margin-left: 324px;
+            width: 246px;
+            height: 306px;
+            margin-top: 108px;
+          "
+        >
           <img
             :src=" winner!.players && winner!.players[0].url   ?winner!.players[0].url  : '/images/u1.jpg'"
-           class="h-[150px]"
             ref="image1"
-         
-           />
-  
-  
+            style="width: 100%; height: 100%"
+          />
         </div>
-        <div class="absolute  w-[117px] h-[100px] left-[-140px]  top-[-45px] rotate-[-15deg]  z-[-2] ">
+        <div
+          class="absolute rotate-[-15deg] z-[-2]"
+          style="
+            width: 243px;
+            margin-right: 329px;
+            height: 291px;
+            margin-top: 100px;
+          "
+        >
           <img
-          class="h-[150px]"
+            style="height: 100%; width: 100%"
             :src=" winner!.players && winner!.players[1].url  ?winner!.players[1].url  : '/images/u2.jpg'"
-          
             ref="image2"
           />
-  
-  
-        </div> 
+        </div>
       </div>
     </div>
-   
-    <WinnerSvg ref="Winnersvg"  />
+
+    <WinnerSvg ref="Winnersvg" />
   </div>
 </template>
 
 <script lang="ts" setup>
-
-
-
 const store = useMyGameStore();
 import gsap from "gsap";
 const { snapshot, game } = storeToRefs(store);
 const { gameService } = store;
 
-const Winnersvg =ref() 
+const Winnersvg = ref();
 const winnerData = ref(null);
-const image2= ref(null);
-const image1= ref(null);
+const image2 = ref(null);
+const image1 = ref(null);
 
 const winnerComp = ref(null);
 
 const scoreMount = () => {
   const t1 = gsap.timeline();
-  t1.delay(1)
-  t1.fromTo([winnerData.value, image1.value,image2.value],{opacity:0}, {
-    duration: 0.75,
-    opacity: 2,
-    ease: "linear",
-  });
+  t1.delay(1);
+  t1.fromTo(
+    [winnerData.value, image1.value, image2.value],
+    { opacity: 0 },
+    {
+      duration: 0.75,
+      opacity: 2,
+      ease: "linear",
+    }
+  );
 };
 
 const scoreUnMount = () => {
@@ -71,7 +110,6 @@ const scoreUnMount = () => {
 
 const { sleep } = useSleep();
 const winner = computed(() => {
-  
   if (game?.value?.winner) {
     if (game.value.winner == "Us") {
       console.log(game.value.usPlayers);
@@ -95,16 +133,15 @@ onMounted(() => {
     if (snapshot.value.matches("winner.intro")) {
       if (Winnersvg.value) {
         scoreMount();
-        Winnersvg.value.enterAnimation()
+        Winnersvg.value.enterAnimation();
 
-        await sleep(5000000000);
+        await sleep(500);
         gameService.send({ type: "NEXT" });
-      
       }
     }
     if (snapshot.value.matches("winner.main")) {
       if (Winnersvg.value) {
-        Winnersvg.value.leaveAnimation()
+        Winnersvg.value.leaveAnimation();
         scoreUnMount();
         await sleep(1500);
         gameService.send({ type: "UPDATE_CONTEXT", ended: false });
@@ -115,7 +152,6 @@ onMounted(() => {
 });
 </script>
 <style scoped>
-
 .winner-comp {
   @apply relative mx-auto mt-[50px];
 }
@@ -123,11 +159,11 @@ onMounted(() => {
   @apply relative z-[-1] left-0 top-0;
 }
 #winner-data {
-  @apply absolute text-4xl opacity-0 ;
+  @apply absolute text-4xl opacity-0;
   font-family: "arefBold";
 }
 #winner-data p {
-  @apply  text-start bg-[linear-gradient(_to_right,#462523_0,#cb9b51_22%,#f6e27a_45%,#f6f2c0_50%,#f6e27a_55%,#cb9b51_78%,#462523_100%_)] text-transparent bg-clip-text m-0 p-0; 
+  @apply text-start bg-[linear-gradient(_to_right,#462523_0,#cb9b51_22%,#f6e27a_45%,#f6f2c0_50%,#f6e27a_55%,#cb9b51_78%,#462523_100%_)] text-transparent bg-clip-text m-0 p-0;
   /* background-color: aqua; */
   -webkit-background-clip: text;
 }
@@ -152,5 +188,4 @@ onMounted(() => {
 /* .image {
   @apply absolute z-[-1] top-0;
 } */
-
 </style>
